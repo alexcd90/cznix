6d2ffaf73af217782b58d01de0efaae945ffc3a7
mac-jiakai908 <jiakai908@163.com>
2022-01-24 14:37:19 +0800

    v0.5-消息封装

--- examples/zx_server/main.go
+++ examples/zx_server/main.go
@@ -11,30 +11,16 @@ type PingRouter struct {
 	znet.BaseRouter //一定要先基础BaseRouter
 }
 
-//Test PreHandle
-func (this *PingRouter) PreHandle(request ziface.IRequest) {
-	fmt.Println("Call Router PreHandle")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping .... \n"))
-	if err != nil {
-		fmt.Println("call back ping ping ping error")
-	}
-}
-
 //Test Handle
 func (this *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call PingRouter Handle")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping\n"))
-	if err != nil {
-		fmt.Println("call back ping ping ping error")
-	}
-}
+	//先读取客户端数据，再回写ping....ping....ping....
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
-//Test PostHandle
-func (this *PingRouter) PostHandle(request ziface.IRequest) {
-	fmt.Println("Call Router PostHandle")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("After ping .....\n"))
+	//回写数据
+	err := request.GetConnection().SendMsg(1, []byte("ping....ping....ping...."))
 	if err != nil {
-		fmt.Println("call back ping ping ping error")
+		fmt.Println(err)
 	}
 }
 
