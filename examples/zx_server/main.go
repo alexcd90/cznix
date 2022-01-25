367aa0bf1014b8d18f8e703cfcd9e48b656c165f
mac-jiakai908 <jiakai908@163.com>
2022-01-25 17:25:11 +0800

    v0.9-链接管理

--- examples/zx_server/main.go
+++ examples/zx_server/main.go
@@ -18,7 +18,7 @@ func (this *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
 	//回写数据
-	err := request.GetConnection().SendMsg(request.GetMsgID(), []byte("ping....ping....ping...."))
+	err := request.GetConnection().SendBuffMsg(request.GetMsgID(), []byte("ping....ping....ping...."))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -34,15 +34,33 @@ func (this *HelloZinxRouter) Handle(request ziface.IRequest) {
 	//先读取客户端的数据，再回写ping...ping...ping
 	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
-	err := request.GetConnection().SendMsg(1, []byte("Hello Zinx Router V0.8"))
+	err := request.GetConnection().SendBuffMsg(1, []byte("Hello Zinx Router V0.9"))
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+//创建连接的时候执行
+func DoConnectionBegin(conn ziface.IConnection) {
+	fmt.Println("DoConnecionBegin is Called ... ")
+	err := conn.SendMsg(2, []byte("DoConnection BEGIN..."))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+//连接断开的时候执行
+func DoConnectionLost(conn ziface.IConnection) {
+	fmt.Println("DoConneciotnLost is Called ... ")
+}
+
 func main() {
 	//创建一个server句柄
 	s := znet.NewServer()
+
+	//注册链接hook回调函数
+	s.SetOnConnStart(DoConnectionBegin)
+	s.SetOnConnStop(DoConnectionLost)
 
 	//配置路由
 	s.AddRouter(0, &PingRouter{})
